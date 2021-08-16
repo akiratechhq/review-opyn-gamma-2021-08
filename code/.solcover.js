9751470d26b3559868b97c63e6bc46955056eb9e
Daniel Luca (@CleanUnicorn) <cleanunicorn@protonmail.com>
2021-08-16 09:47:12 +0000

    🤹‍♀️ Init review

---
+++ code/.solcover.js
@@ -0,0 +1,13 @@
+module.exports = {
+  client: require('ganache-cli'),
+  providerOptions: {
+    mnemonic: "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
+  },
+  silent: true,
+  skipFiles: [
+    'Migrations.sol',
+    'mocks/',
+    'packages/',
+    'external/canonical-weth'
+  ]
+}
